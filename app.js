ac29fd301de672b5b09b0de1c08bd222051db2c8
zxcataki <101623970+myupi@users.noreply.github.com>
2022-12-20 23:42:53 +0500

    Update app.js

--- app.js
+++ app.js
@@ -12,11 +12,8 @@ setInterval(() =>{
     let soniya = now.getSeconds() * burchak;
 
     hr.style.transform = `rotateZ(${soat + daqiqa / 12}deg)`;
-    hr.style.transition = `1s linear`;
     mn.style.transform = `rotateZ(${daqiqa}deg)`;
-    mn.style.transition = `1s linear`;
     sc.style.transform = `rotateZ(${soniya}deg)`;
-    sc.style.transition = `1s linear`;
 
 })
 
